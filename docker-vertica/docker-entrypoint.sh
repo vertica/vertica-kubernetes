4d7201a9b302305bdd0e9190eee7ef44c2b2ed26
Matt Spilchen <matt.spilchen@microfocus.com>
2021-07-08 14:29:47 -0400

    Save contents of /opt/vertica/config/licensing
    When running the vertica-k8s container all of the contents in
    /opt/vertica/config are lost on initial startup because we use a PV to
    back that.  There are certain files that we want to keep and must be
    copied back over when the container starts.  We already stored the
    contents of /opt/vertica/config/licensing in /home/dbadmin/licensing/ce,
    we are going to copy that directory over each time the container starts.

--- docker-vertica/docker-entrypoint.sh
+++ docker-vertica/docker-entrypoint.sh
@@ -6,17 +6,22 @@ start_cron(){
     sudo /usr/sbin/crond
 }
 
-# We copy back the logrotate files in their original location /opt/vertica/config/
-# that's because we have a Persistent Volume that backs /opt/vertica/config, so
+# We copy back the files normally stored in /opt/vertica/config/.  We do this
+# because we have a Persistent Volume that backs /opt/vertica/config, so
 # it starts up empty and must be populated
-copy_logrotate_files(){
+copy_config_files() {
     # We must use sudo in case the PV was created with permissions less than 0777.
     sudo cp -r /home/dbadmin/logrotate/* /opt/vertica/config/
     rm -rf /home/dbadmin/logrotate
+
+    sudo mkdir -p /opt/vertica/config/licensing
+    sudo cp -r /home/dbadmin/licensing/ce/* /opt/vertica/config/licensing
+    sudo chown -R dbadmin:verticadba /opt/vertica/config/licensing
+    sudo chmod -R 0777 /opt/vertica/config/licensing
 }
 
 start_cron
-copy_logrotate_files
+copy_config_files
 
 echo "Vertica container is now running"
 sudo /usr/sbin/sshd -D
