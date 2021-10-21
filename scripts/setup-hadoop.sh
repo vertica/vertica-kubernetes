19b13b3237ccd8c710d4d3e73345ba5508aa8f75
Matt Spilchen <matt.spilchen@microfocus.com>
2021-10-20 21:42:03 -0400

    Prep for hdfs ci

---
+++ scripts/setup-hadoop.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# (c) Copyright [2021] Micro Focus or one of its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# A script that will setup hadoop for use with e2e tests
+
+set -o errexit
+set -o pipefail
+
+HADOOP_NS=kuttl-e2e-hadoop
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+REPO_DIR=$(dirname $SCRIPT_DIR)
+TIMEOUT=600
+RELEASE=hdfs-ci
+CHART=vertica-charts/hdfs-ci
+DEFCHART=$CHART
+
+function usage {
+    echo "usage: $0 [-u] [-t <seconds>] [-c <chart>]"
+    echo
+    echo "Options:"
+    echo "  -t <seconds>  Length of the timeout."
+    echo "  -c <chart>    Override the name of the chart to use."
+    echo
+    exit 1
+}
+
+OPTIND=1
+while getopts "ht:c:" opt; do
+    case ${opt} in
+        h)
+            usage
+            ;;
+        t)
+            TIMEOUT=$OPTARG
+            ;;
+        c)
+            CHART=$OPTARG
+            ;;
+        \?)
+            echo "Unknown option: -${opt}"
+            usage
+            ;;
+    esac
+done
+
+set -o xtrace
+kubectl delete namespace $HADOOP_NS || :
+kubectl create namespace $HADOOP_NS
+
+if [[ "$CHART" == "$DEFCHART" ]]
+then
+    helm repo add vertica-charts https://vertica.github.io/charts
+    helm repo update
+fi
+
+helm install --wait -n $HADOOP_NS $RELEASE $CHART --timeout ${TIMEOUT}s
