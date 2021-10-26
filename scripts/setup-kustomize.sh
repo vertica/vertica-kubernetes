337a0cca1d4c5fa0d57b8a0258663fe21bfd952a
Matt Spilchen <matt.spilchen@microfocus.com>
2021-10-25 21:59:15 -0400

    Move tests that don't run in azurite to e2e-extra

--- scripts/setup-kustomize.sh
+++ scripts/setup-kustomize.sh
@@ -630,11 +630,11 @@ setup_creds_for_create_s3_bucket
 
 # Descend into each test and create the overlay kustomization.
 # The overlay is created in a directory like: overlay/<tc-name>
-for tdir in e2e/*/*/base
+for tdir in e2e/*/*/base e2e-extra/*/*/base
 do
     create_vdb_pod_kustomization $(dirname $tdir) $(basename $(realpath $tdir/../..))
 done
-for tdir in e2e/* e2e-disabled/*
+for tdir in e2e/* e2e-extra/* e2e-disabled/*
 do
     clean_communal_kustomization $tdir
 done
