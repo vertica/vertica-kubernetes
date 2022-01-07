05e61d6f286647b36451e8b699c43476683b998e
Matt Spilchen <matt.spilchen@microfocus.com>
2022-01-06 21:17:37 -0400

    Update copyright

--- scripts/patch-image-in-vdb.sh
+++ scripts/patch-image-in-vdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# (c) Copyright [2021] Micro Focus or one of its affiliates.
+# (c) Copyright [2022] Micro Focus or one of its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
