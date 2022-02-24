0d3e47d50105c77d9628eb0dcd09c318e59ed594
Naren <narendra.prabhu@microfocus.com>
2022-02-24 16:55:11 -0500

    Change the test endpoint name

--- pkg/controllers/init_db_test.go
+++ pkg/controllers/init_db_test.go
@@ -206,7 +206,7 @@ var _ = Describe("init_db", func() {
 		Expect(getEndpointHostPort("http://hostname")).Should(Equal("hostname"))
 		Expect(getEndpointHostPort("https://tlsHost:3000")).Should(Equal("tlsHost:3000"))
 		Expect(getEndpointHostPort("account@myhost")).Should(Equal("account@myhost"))
-		Expect(getEndpointHostPort("azb://mspilchen/db/")).Should(Equal("mspilchen/db"))
+		Expect(getEndpointHostPort("azb://account/container/db/")).Should(Equal("account/container/db"))
 
 	})
 
