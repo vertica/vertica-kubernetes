c714bbb20938c6294325fdfbeba39981b856c08c
Naren <narendra.prabhu@microfocus.com>
2022-03-08 17:31:53 -0500

    Add errors package to supplement changes to reconcile error handling

---
+++ pkg/errors/errors.go
@@ -0,0 +1,30 @@
+/*
+ (c) Copyright [2021-2022] Micro Focus or one of its affiliates.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package errors
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+// Checks if the reconcile function returned an error,
+// or if a requeue is necessary
+// or if the requeueAfter is set
+func IsReconcileAborted(res ctrl.Result, err error) bool {
+	if res.Requeue || err != nil || res.RequeueAfter > 0 {
+		return true
+	}
+	return false
+}
