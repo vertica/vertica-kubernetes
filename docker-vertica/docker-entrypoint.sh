a4bc5dd55b163e0d13522c98147ec7f9bd98ae5d
Duane Bronson <Duane.Bronson@microfocus.com>
2022-03-17 09:13:23 -0400

    removing the "rm" on the directory

--- docker-vertica/docker-entrypoint.sh
+++ docker-vertica/docker-entrypoint.sh
@@ -13,7 +13,6 @@ copy_config_files() {
     mkdir -p /opt/vertica/config/licensing
 
     mv /home/dbadmin/logrotate/* /opt/vertica/config/ 2>/dev/null || true
-    rm -rf /home/dbadmin/logrotate
 
     cp -r /home/dbadmin/licensing/ce/* /opt/vertica/config/licensing 2>/dev/null || true
     chmod -R ugo+r,u+rw /opt/vertica/config/licensing
