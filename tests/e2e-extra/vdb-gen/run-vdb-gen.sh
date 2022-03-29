c7eca0e5fa82a72d6bca7d12064f6a58fb5987c7
roypaulin <55731470+roypaulin@users.noreply.github.com>
2022-03-29 17:00:13 +0200

    Change vertica-k8s base image to ubuntu (#187)
    This PR replaces the base image of the vertica-k8s. It was CentOS 7 before and
    now it uses Ubuntu focal. This was done to address security vulnerabilities
    that were found by scanners.  It also ends up in having a smaller image.

--- tests/e2e-extra/vdb-gen/run-vdb-gen.sh
+++ tests/e2e-extra/vdb-gen/run-vdb-gen.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # (c) Copyright [2021-2022] Micro Focus or one of its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
