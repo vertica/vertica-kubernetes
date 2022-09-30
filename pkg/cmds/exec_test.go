9a7359e005c4006a6c20c952fa1824559e473d39
spilchen <matt.spilchen@vertica.com>
2022-09-29 21:40:36 -0300

    Hide communal credentials from the operator log (#259)
    This obfuscates the communal credentials when logging commands that the
    operator runs.

---
+++ pkg/cmds/exec_test.go
@@ -0,0 +1,35 @@
+/*
+ (c) Copyright [2021-2022] Micro Focus or one of its affiliates.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package cmds
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("exec", func() {
+	It("should obfuscate password", func() {
+		s := generateLogOutput("vsql", "--password", "silly", "-c", "select 1")
+		Expect(s).Should(Equal("vsql --password ******* -c select 1 "))
+	})
+
+	It("should obfuscate credentials", func() {
+		s := generateLogOutput(`cat > auth_parms.conf<<< '
+awsauth = user:pass
+awsendpoint = minio`)
+		Expect(s).Should(Equal("cat > auth_parms.conf<<< '\nawsauth = ****\nawsendpoint = minio "))
+	})
+})
