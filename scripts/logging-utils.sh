afae50be2a5856e64eea354ae9aa936c53abe88e
Matt Spilchen <matt.spilchen@vertica.com>
2022-12-01 14:35:12 -0400

    Add script to change the operator release version

---
+++ scripts/logging-utils.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# (c) Copyright [2021-2022] Micro Focus or one of its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Utilities to be sourced into various bash scripts
+
+CYAN='\033[0;36m'
+RED='\033[1;31m'
+NC='\033[0m'  # No color
+
+function logGeneric {
+    printf $1
+    shift
+    printf $(date "+%D")
+    printf " "
+    printf $(date "+%T")
+    printf " [$1]  "
+    shift
+    for i in $*; do
+        printf "$i "
+    done
+    printf "$NC\n"
+
+}
+
+function logInfo {
+    logGeneric ${CYAN} "ℹ" $@
+}
+
+function logError {
+    logGeneric ${RED} "!" $@
+}
