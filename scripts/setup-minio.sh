dbcf7777541715f5aa91d249301f3f5360d9dd4c
Matt Spilchen <matt.spilchen@microfocus.com>
2023-01-16 11:16:59 -0400

    Use newer version of minio in e2e/CI

--- scripts/setup-minio.sh
+++ scripts/setup-minio.sh
@@ -58,9 +58,9 @@ set -o xtrace
 
 # First setup the operator
 kubectl krew update
-kubectl krew install --manifest-url https://raw.githubusercontent.com/kubernetes-sigs/krew-index/9ee1af89f729b999bcd37f90484c4d74c70a1df2/plugins/minio.yaml
+kubectl krew install --manifest-url https://raw.githubusercontent.com/kubernetes-sigs/krew-index/d1817869b86fd040a923682b1392bdb232947bf5/plugins/minio.yaml
 # If these images ever change, they must be updated in tests/external-images-s3-ci.txt
-kubectl minio init --console-image minio/console:v0.9.8 --image minio/operator:v4.2.7
+kubectl minio init --console-image minio/console:v0.22.5 --image minio/operator:v4.5.7
 
 # The above command will create the CRD.  But there is a timing hole where the
 # CRD is not yet registered with k8s, causing the tenant creation below to
