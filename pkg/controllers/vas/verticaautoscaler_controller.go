da5b4f0aa772f8e146100de12eba0d3555f2af05
Roy Paulin <roy.paulin@vertica.com>
2023-04-14 13:52:23 +0200

    Update license years

--- pkg/controllers/vas/verticaautoscaler_controller.go
+++ pkg/controllers/vas/verticaautoscaler_controller.go
@@ -1,5 +1,5 @@
 /*
-Copyright [2021-2022] Open Text.
+Copyright [2021-2023] Open Text.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
