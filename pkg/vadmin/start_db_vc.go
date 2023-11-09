ae5bbb6cf14d6ea45c830dd46fe3d2440c9a0a78
roypaulin <roy.paulin@microfocus.com>
2023-11-09 19:36:39 +0100

    Update SartDB

--- pkg/vadmin/start_db_vc.go
+++ pkg/vadmin/start_db_vc.go
@@ -89,6 +89,7 @@ func (v *VClusterOps) genStartDBOptions(s *startdb.Parms, certs *HTTPSCerts) (vo
 	*opts.HonorUserInput = true
 
 	// timeout option
-	opts.StatePollingTimeout = v.VDB.GetRestartTimeout()
+	opts.StatePollingTimeout = new(int)
+	*opts.StatePollingTimeout = v.VDB.GetRestartTimeout()
 	return opts, nil
 }
