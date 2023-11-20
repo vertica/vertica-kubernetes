14af7f3c6ef8bec3a635728d6f5fd3d60c448fad
roypaulin <roy.paulin@microfocus.com>
2023-11-20 15:52:17 +0100

    Update comment

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -49,6 +49,10 @@ var UpgradePaths = map[Components]Info{
 	{12, 0, 2}: {"v23.3.x", Components{23, 3, 0}},
 	{12, 0, 3}: {"v23.3.x", Components{23, 3, 0}},
 	{12, 0, 4}: {"v23.3.x", Components{23, 3, 0}},
+	// From here upwards the x.4.0 release (4th quarter release)
+	// is the LTS version so all releases will have the closest x.4.0
+	// as their next release they can be upgraded to. If the next x.4.0
+	// is not out yet then it will be the latest release.
 	{23, 3, 0}: {"v23.4.x", Components{23, 4, 0}},
 }
 
