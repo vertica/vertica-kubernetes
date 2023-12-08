7f141454fd490ebb0c04a38f69b1c6e78e3de8f9
spilchen <mspilchen@opentext.com>
2023-12-07 21:50:05 -0400

    Switched server container from ubuntu to rockylinux (#624)

---
+++ tests/e2e-leg-6/verify-server-logrotate/setup-env-runtime.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o xtrace
+
+for i in $(seq 1 5)
+do
+    # v1 images only have sudo and no root password. v2 images don't have sudo but
+    # set a hard coded password for root. This scripts works for both images.
+    if which sudo  # v1 image
+    then
+        if which apt-get # ubuntu / apt-get
+        then
+           echo "Nothing for ubuntu images"
+        else # yum package manager
+           sudo yum install -y diffutils && break || sleep 60
+        fi
+    else  # v2 image
+        if which apt-get # ubuntu / apt-get
+        then
+           echo "Nothing for ubuntu images"
+        else # yum package manager
+            echo root | su root sh -c 'yum install -y diffutils' && break || sleep 60
+        fi
+    fi
+done
