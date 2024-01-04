c33e209d54e2a972006e3caa2301d9f628482170
spilchen <mspilchen@opentext.com>
2024-01-04 17:04:36 -0400

    VCluster API refresh: allow create db to proceed if db is already running (#652)
    This builds on a change made in vclusterops. It can now emit a specific
    error if it discovers that the database is already running. We add a
    feature flag to tolerate this (the default is true). This can allow us
    to proceed if we are in an environment with few resources available,
    which can cause instability during the creation process. If we have
    previously progressed far enough that Vertica is running, we will skip
    the create db step and proceed to the next reconciler. This will update
    the status condition to indicate that the database has been initialized.

--- pkg/vadmin/restart_node_vc.go
+++ pkg/vadmin/restart_node_vc.go
@@ -54,21 +54,17 @@ func (v *VClusterOps) RestartNode(ctx context.Context, opts ...restartnode.Optio
 func (v *VClusterOps) genStartNodeOptions(s *restartnode.Parms, certs *HTTPSCerts) *vops.VStartNodesOptions {
 	su := v.VDB.GetVerticaUser()
 	honorUserInput := true
-	opts := vops.VStartNodesOptions{
-		DatabaseOptions: vops.DatabaseOptions{
-			DBName:         &v.VDB.Spec.DBName,
-			RawHosts:       []string{s.InitiatorIP},
-			Ipv6:           vstruct.MakeNullableBool(net.IsIPv6(s.InitiatorIP)),
-			Key:            certs.Key,
-			Cert:           certs.Cert,
-			CaCert:         certs.CaCert,
-			UserName:       &su,
-			Password:       &v.Password,
-			HonorUserInput: &honorUserInput,
-		},
-		Nodes: s.RestartHosts,
-	}
-	// timeout option
+	opts := vops.VStartNodesOptionsFactory()
+	opts.DBName = &v.VDB.Spec.DBName
+	opts.RawHosts = []string{s.InitiatorIP}
+	opts.Ipv6 = vstruct.MakeNullableBool(net.IsIPv6(s.InitiatorIP))
+	opts.Key = certs.Key
+	opts.Cert = certs.Cert
+	opts.CaCert = certs.CaCert
+	opts.UserName = &su
+	opts.Password = &v.Password
+	opts.HonorUserInput = &honorUserInput
+	opts.Nodes = s.RestartHosts
 	opts.StatePollingTimeout = v.VDB.GetRestartTimeout()
 	return &opts
 }
