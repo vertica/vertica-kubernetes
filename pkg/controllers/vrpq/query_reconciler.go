b6bfb0110d1ae216541695311b1c94a3e36dca2b
chinhtranvan <tranc@cua.edu>
2024-01-25 12:39:25 -0500

    WIP

---
+++ pkg/controllers/vrpq/query_reconciler.go
@@ -0,0 +1,144 @@
+/*
+ (c) Copyright [2021-2023] Open Text.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package vrpq
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	v1 "github.com/vertica/vertica-kubernetes/api/v1"
+	vapi "github.com/vertica/vertica-kubernetes/api/v1beta1"
+	"github.com/vertica/vertica-kubernetes/pkg/controllers"
+	verrors "github.com/vertica/vertica-kubernetes/pkg/errors"
+	restorepointsquery "github.com/vertica/vertica-kubernetes/pkg/vadmin/opts/restorepointsquery"
+	config "github.com/vertica/vertica-kubernetes/pkg/vdbconfig"
+	vrpqstatus "github.com/vertica/vertica-kubernetes/pkg/vrpqstatus"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+type QueryReconciler struct {
+	VRec           *VerticaRestorePointsQueryReconciler
+	Vrpq           *vapi.VerticaRestorePointsQuery
+	Log            logr.Logger
+	InitiatorPod   types.NamespacedName // The pod that we run admin commands from
+	InitiatorPodIP string               // The IP of the initiating pod
+	config.ConfigParamsGenerator
+}
+
+func MakeRestorePointsQueryReconciler(r *VerticaRestorePointsQueryReconciler, vrpq *vapi.VerticaRestorePointsQuery,
+	log logr.Logger) controllers.ReconcileActor {
+	return &QueryReconciler{
+		VRec: r,
+		Vrpq: vrpq,
+		Log:  log.WithName("QueryReconciler"),
+		ConfigParamsGenerator: config.ConfigParamsGenerator{
+			VRec: r,
+			Log:  log.WithName("QueryReconciler"),
+		},
+	}
+}
+
+func (q *QueryReconciler) Reconcile(ctx context.Context, req *ctrl.Request) (ctrl.Result, error) {
+	// no-op if QueryComplete is true
+	isSet := q.Vrpq.IsStatusConditionTrue(vapi.Querying)
+	if isSet {
+		return ctrl.Result{}, nil
+	}
+	// collect information from a VerticaDB.
+	if res, err := q.collectInfoFromVdb(ctx); verrors.IsReconcileAborted(res, err) {
+		return res, err
+	}
+
+	return ctrl.Result{}, q.runListRestorePoints(ctx, req)
+}
+
+// fetch the VerticaDB and collect access information to the communal storage for the VerticaRestorePointsQuery CR,
+func (q *QueryReconciler) collectInfoFromVdb(ctx context.Context) (ctrl.Result, error) {
+	vdb := &v1.VerticaDB{}
+	res := ctrl.Result{}
+
+	opts := []restorepointsquery.Option{
+		restorepointsquery.WithInitiator(q.InitiatorPod, q.InitiatorPodIP),
+	}
+
+	err := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		var e error
+		if res, e = fetchVDB(ctx, q.VRec, q.Vrpq, vdb); verrors.IsReconcileAborted(res, e) {
+			return e
+		}
+		q.Vdb = vdb
+		// build communal storage params if there is not one
+		if q.ConfigurationParams == nil {
+			res, e = q.ConstructConfigParms(ctx)
+			if verrors.IsReconcileAborted(res, e) {
+				return e
+			}
+		}
+		// extract out the communal and config information to pass down to the vclusterops API.
+		opts = append(opts,
+			restorepointsquery.WithCommunalPath(vdb.GetCommunalPath()),
+			restorepointsquery.WithConfigurationParams(q.ConfigurationParams.GetMap()),
+		)
+
+		return nil
+	})
+
+	return res, err
+}
+
+// setListRestorePointsQueryConditions will update the status condition before and after calling
+// list restore points api
+// Temporarily, runListRestorePoints will not call the ListRestorePoints API
+// since the dispatcher is not set up yet
+func (q *QueryReconciler) runListRestorePoints(ctx context.Context, _ *ctrl.Request) error {
+	// update query message prior to calling vclusterops API
+	err := vrpqstatus.UpdateMessageStatus(ctx, q.VRec.Client, q.VRec.Log, q.Vrpq,
+		"Query is still in progress")
+	if err != nil {
+		return err
+	}
+
+	// set Querying status condition prior to calling vclusterops API
+	err = vrpqstatus.UpdateCondition(ctx, q.VRec.Client, q.VRec.Log, q.Vrpq,
+		v1.MakeCondition(vapi.Querying, metav1.ConditionTrue, ""))
+	if err != nil {
+		return err
+	}
+
+	// API should be called to proceed here
+
+	// clear Querying status condition
+	err = vrpqstatus.UpdateCondition(ctx, q.VRec.Client, q.VRec.Log, q.Vrpq,
+		v1.MakeCondition(vapi.Querying, metav1.ConditionFalse, ""))
+	if err != nil {
+		return err
+	}
+
+	// update query message after calling vclusterops API successfully
+	err = vrpqstatus.UpdateMessageStatus(ctx, q.VRec.Client, q.VRec.Log, q.Vrpq,
+		"Query has completed successfully")
+	if err != nil {
+		return err
+	}
+
+	// set the QueryComplete if the vclusterops API succeeded
+	return vrpqstatus.UpdateCondition(ctx, q.VRec.Client, q.VRec.Log, q.Vrpq,
+		v1.MakeCondition(vapi.QueryComplete, metav1.ConditionTrue, ""))
+}
---
+++ pkg/vrpqstatus/status_test.go
@@ -0,0 +1,209 @@
+/*
+ (c) Copyright [2021-2023] Open Text.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package vrpqstatus
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/go-logr/logr"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	gtypes "github.com/onsi/gomega/types"
+	v1 "github.com/vertica/vertica-kubernetes/api/v1"
+	vapi "github.com/vertica/vertica-kubernetes/api/v1beta1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+var k8sClient client.Client
+var testEnv *envtest.Environment
+var logger logr.Logger
+
+var _ = BeforeSuite(func() {
+	logger = zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true))
+	logf.SetLogger(logger)
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	cfg, err := testEnv.Start()
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+	ExpectWithOffset(1, cfg).NotTo(BeNil())
+	restCfg := cfg
+
+	err = vapi.AddToScheme(scheme.Scheme)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+
+	k8sClient, err = client.New(restCfg, client.Options{Scheme: scheme.Scheme})
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+})
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecs(t, "vrpqstatus Suite")
+}
+
+func EqualVerticaRestorePointsQueryCondition(expected interface{}) gtypes.GomegaMatcher {
+	return &representVerticaRestorePointsQueryCondition{
+		expected: expected,
+	}
+}
+
+type representVerticaRestorePointsQueryCondition struct {
+	expected interface{}
+}
+
+func (matcher *representVerticaRestorePointsQueryCondition) Match(actual interface{}) (success bool, err error) {
+	response, ok := actual.(metav1.Condition)
+	if !ok {
+		return false, fmt.Errorf("representVerticaRestorePointsQueryCondition matcher expects a vapi.VerticaRestorePointsQueryCondition")
+	}
+	expectedObj, ok := matcher.expected.(metav1.Condition)
+	if !ok {
+		return false, fmt.Errorf("representVerticaRestorePointsQueryCondition should compare with a metav1.Condition")
+	}
+	// Compare everything except lastTransitionTime
+	return response.Type == expectedObj.Type && response.Status == expectedObj.Status, nil
+}
+
+func (matcher *representVerticaRestorePointsQueryCondition) FailureMessage(actual interface{}) (message string) {
+	return fmt.Sprintf("Expected\n\t%#v\nto equal\n\t%#v", actual, matcher.expected)
+}
+
+func (matcher *representVerticaRestorePointsQueryCondition) NegatedFailureMessage(actual interface{}) (message string) {
+	return fmt.Sprintf("Expected\n\t%#v\nto not equal\n\t%#v", actual, matcher.expected)
+}
+
+var _ = Describe("status", func() {
+	ctx := context.Background()
+
+	It("should update status condition when no conditions have been set", func() {
+		vrpq := vapi.MakeVrpq()
+		Expect(k8sClient.Create(ctx, vrpq)).Should(Succeed())
+		defer func() { Expect(k8sClient.Delete(ctx, vrpq)).Should(Succeed()) }()
+
+		cond := v1.MakeCondition(vapi.Querying, metav1.ConditionTrue, "")
+		Expect(UpdateCondition(ctx, k8sClient, logger, vrpq, cond)).Should(Succeed())
+		fetchVdb := &vapi.VerticaRestorePointsQuery{}
+		nm := types.NamespacedName{Namespace: vrpq.Namespace, Name: vrpq.Name}
+		Expect(k8sClient.Get(ctx, nm, fetchVdb)).Should(Succeed())
+		for _, v := range []*vapi.VerticaRestorePointsQuery{vrpq, fetchVdb} {
+			Expect(len(v.Status.Conditions)).Should(Equal(1))
+			Expect(v.Status.Conditions[0]).Should(EqualVerticaRestorePointsQueryCondition(*cond))
+		}
+	})
+
+	It("should be able to change an existing status condition", func() {
+		vrpq := vapi.MakeVrpq()
+		Expect(k8sClient.Create(ctx, vrpq)).Should(Succeed())
+		defer func() { Expect(k8sClient.Delete(ctx, vrpq)).Should(Succeed()) }()
+
+		conds := []metav1.Condition{
+			{Type: vapi.Querying, Status: metav1.ConditionTrue, Reason: v1.UnknownReason},
+			{Type: vapi.Querying, Status: metav1.ConditionFalse, Reason: v1.UnknownReason},
+		}
+		for i := range conds {
+			Expect(UpdateCondition(ctx, k8sClient, logger, vrpq, &conds[i])).Should(Succeed())
+			fetchVdb := &vapi.VerticaRestorePointsQuery{}
+			nm := types.NamespacedName{Namespace: vrpq.Namespace, Name: vrpq.Name}
+			Expect(k8sClient.Get(ctx, nm, fetchVdb)).Should(Succeed())
+			for _, v := range []*vapi.VerticaRestorePointsQuery{vrpq, fetchVdb} {
+				Expect(len(v.Status.Conditions)).Should(Equal(1))
+				Expect(v.Status.Conditions[0]).Should(EqualVerticaRestorePointsQueryCondition(conds[i]))
+			}
+		}
+	})
+
+	It("should be able to handle multiple status conditions", func() {
+		vrpq := vapi.MakeVrpq()
+		Expect(k8sClient.Create(ctx, vrpq)).Should(Succeed())
+		defer func() { Expect(k8sClient.Delete(ctx, vrpq)).Should(Succeed()) }()
+
+		conds := []metav1.Condition{
+			{Type: vapi.Querying, Status: metav1.ConditionTrue, Reason: v1.UnknownReason},
+			{Type: vapi.Querying, Status: metav1.ConditionFalse, Reason: v1.UnknownReason},
+			{Type: vapi.QueryComplete, Status: metav1.ConditionTrue, Reason: v1.UnknownReason},
+		}
+
+		for i := range conds {
+			Expect(UpdateCondition(ctx, k8sClient, logger, vrpq, &conds[i])).Should(Succeed())
+		}
+
+		fetchVdb := &vapi.VerticaRestorePointsQuery{}
+		nm := types.NamespacedName{Namespace: vrpq.Namespace, Name: vrpq.Name}
+		Expect(k8sClient.Get(ctx, nm, fetchVdb)).Should(Succeed())
+		for _, v := range []*vapi.VerticaRestorePointsQuery{vrpq, fetchVdb} {
+			Expect(len(v.Status.Conditions)).Should(Equal(2))
+			Expect(v.Status.Conditions[0]).Should(EqualVerticaRestorePointsQueryCondition(conds[1]))
+			Expect(v.Status.Conditions[1]).Should(EqualVerticaRestorePointsQueryCondition(conds[2]))
+		}
+	})
+
+	It("should change the lastTransitionTime when a status condition is changed", func() {
+		vrpq := vapi.MakeVrpq()
+		Expect(k8sClient.Create(ctx, vrpq)).Should(Succeed())
+		defer func() { Expect(k8sClient.Delete(ctx, vrpq)).Should(Succeed()) }()
+		origTime := metav1.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC)
+		Expect(UpdateCondition(ctx, k8sClient, logger, vrpq,
+			&metav1.Condition{Type: vapi.Querying, Status: metav1.ConditionFalse, LastTransitionTime: origTime,
+				Reason: v1.UnknownReason},
+		)).Should(Succeed())
+		Expect(UpdateCondition(ctx, k8sClient, logger, vrpq,
+			&metav1.Condition{Type: vapi.Querying, Status: metav1.ConditionTrue, LastTransitionTime: origTime,
+				Reason: v1.UnknownReason},
+		)).Should(Succeed())
+		Expect(vrpq.Status.Conditions[0].LastTransitionTime).ShouldNot(Equal(origTime))
+	})
+
+	It("should update the message status", func() {
+		vrpq := vapi.MakeVrpq()
+		Expect(k8sClient.Create(ctx, vrpq)).Should(Succeed())
+		defer func() { Expect(k8sClient.Delete(ctx, vrpq)).Should(Succeed()) }()
+		msg := "Query is still in progress"
+		msg1 := "Query has completed successfully"
+
+		Expect(UpdateMessageStatus(ctx, k8sClient, logger, vrpq, msg)).Should(Succeed())
+
+		nm := types.NamespacedName{Namespace: vrpq.Namespace, Name: vrpq.Name}
+		Expect(k8sClient.Get(ctx, nm, vrpq)).Should(Succeed())
+		Expect(vrpq.Status.Message).Should(Equal(msg))
+
+		Expect(UpdateMessageStatus(ctx, k8sClient, logger, vrpq, msg1)).Should(Succeed())
+
+		Expect(k8sClient.Get(ctx, nm, vrpq)).Should(Succeed())
+		Expect(vrpq.Status.Message).Should(Equal(msg1))
+	})
+})
