vertica/vertica-kubernetes

commit f29adbde891271dc388079ba4013f39b102de055

Fix intermittent e2e test failures (#750)
Partially back out the change in #745. This is another attempt at
getting state of the containers pids right after pods have been
rescheduled during upgrade. This is to help fix an intermittent e2e test
failure.

pkg/controllers/vdb/podfacts.go
@@ -199,6 +199,9 @@ type GatherState struct {
 	FileExists             map[string]bool `json:"fileExists"`
 	DBExists               bool            `json:"dbExists"`
 	VerticaPIDRunning      bool            `json:"verticaPIDRunning"`
+	VerticaProcess         string          `json:"verticaProcess"`
+	SpreadPIDRunning       bool            `json:"spreadPIDRunning"`
+	SpreadProcess          string          `json:"spreadProcess"`
 	UpNode                 bool            `json:"upNode"`
 	StartupComplete        bool            `json:"startupComplete"`
 	Compat21NodeName       string          `json:"compat21NodeName"`
@@ -433,11 +436,18 @@ func (p *PodFacts) genGatherScript(vdb *vapi.VerticaDB, pf *PodFact) string {
 	// The output of the script is yaml. We use a yaml package to unmarshal the
 	// output directly into a GatherState struct. And changes to this script
 	// must have a corresponding change in GatherState.
-	// Note: we replaced ^vertica with ^.*vertica\s-D as pattern to get vertica process
-	// because we need it when vertica and NMA are in separate containers. We plan to
-	// remove it and use vertica process only for admintools after VER-91286
+	script := strings.Builder{}
+	script.WriteString(p.genGatherScriptBase(vdb, pf))
+	if pf.execContainerName == names.ServerContainer {
+		script.WriteString(p.genGatherScriptForVerticaPIDCollection())
+	}
+	return script.String()
+}
+
+func (p *PodFacts) genGatherScriptBase(vdb *vapi.VerticaDB, pf *PodFact) string {
 	return dedent.Dedent(fmt.Sprintf(`
 		set -o errexit
+		set -o pipefail
 		echo -n 'installIndicatorExists: '
 		test -f %s && echo true || echo false
 		echo -n 'eulaAccepted: '
@@ -469,8 +479,6 @@ func (p *PodFacts) genGatherScript(vdb *vapi.VerticaDB, pf *PodFact) string {
 		test -f %s && echo -n '"' && echo -n $(cat %s) && echo '"' || echo '""'
 		echo -n 'vnodeName: '
 		cd %s/%s/v_%s_node????_catalog 2> /dev/null && basename $(pwd) | rev | cut -c9- | rev || echo ""
-		echo -n 'verticaPIDRunning: '
-		[[ $(pgrep -f "^.*vertica\s-D" --runstates RDS) ]] && echo true || echo false
 		echo -n 'upNode: '
 		%s 2> /dev/null | grep --quiet 200 2> /dev/null && echo true || echo false
 		echo -n 'startupComplete: '
@@ -502,6 +510,22 @@ func (p *PodFacts) genGatherScript(vdb *vapi.VerticaDB, pf *PodFact) string {
 		pf.catalogPath,
 		pf.catalogPath,
 	))
+}
+
+func (p *PodFacts) genGatherScriptForVerticaPIDCollection() string {
+	// This is only be run when vertica process is running in the container we
+	// are doing the gather. And this is only true if we aren't running the nma
+	// in a separate container.
+	return dedent.Dedent(`
+		echo -n 'verticaPIDRunning: '
+		[[ $(pgrep -f "^.*vertica\s-D") ]] && echo true || echo false
+		echo -n 'verticaProcess: '
+		pgrep -f "^.*vertica\s-D" -a | tail -1 || echo error
+		echo -n 'spreadPIDRunning: '
+		[[ $(pgrep spread) ]] && echo true || echo false
+		echo -n 'spreadProcess: '
+		pgrep spread -a | tail -1 || echo error
+ 	`)
 }
 
 // getPathToVerifyCatalogExists will return a suffix of a path that we can check
@@ -689,7 +713,7 @@ func (p *PodFact) setNodeState(gs *GatherState, useVclusterOps bool) {
 	// At one point, we ran a query against the nodes table. But it became
 	// tricker to decipher what query failure meant -- is vertica down or is it
 	// a problem with the query?
-	p.upNode = p.dbExists && gs.VerticaPIDRunning
+	p.upNode = p.dbExists && gs.VerticaPIDRunning && gs.SpreadPIDRunning
 }
 
 // checkDCTableAnnotations will check if the pod has the necessary annotations

pkg/controllers/vdb/podfacts_test.go
@@ -161,7 +161,7 @@ var _ = Describe("podfacts", func() {
 		fpr := &cmds.FakePodRunner{}
 		pfs := MakePodFacts(vdbRec, fpr)
 		pf := &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs := &GatherState{VerticaPIDRunning: true}
+		gs := &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true}
 		Expect(pfs.checkForSimpleGatherStateMapping(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.upNode).Should(BeTrue())
 	})
@@ -179,7 +179,7 @@ var _ = Describe("podfacts", func() {
 		}
 		pfs := MakePodFacts(vdbRec, fpr)
 		pf := &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs := &GatherState{VerticaPIDRunning: true}
+		gs := &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true}
 		Expect(pfs.checkForSimpleGatherStateMapping(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pfs.checkNodeStatus(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.upNode).Should(BeTrue())
@@ -237,17 +237,17 @@ var _ = Describe("podfacts", func() {
 		fpr := &cmds.FakePodRunner{}
 		pfs := MakePodFacts(vdbRec, fpr)
 		pf := &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs := &GatherState{VerticaPIDRunning: true, StartupComplete: false}
+		gs := &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true, StartupComplete: false}
 		Expect(pfs.checkIfNodeIsDoingStartup(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.startupInProgress).Should(BeTrue())
 
 		pf = &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs = &GatherState{VerticaPIDRunning: false, StartupComplete: true}
+		gs = &GatherState{VerticaPIDRunning: false, SpreadPIDRunning: false, StartupComplete: true}
 		Expect(pfs.checkIfNodeIsDoingStartup(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.startupInProgress).Should(BeFalse())
 
 		pf = &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs = &GatherState{VerticaPIDRunning: true, StartupComplete: true}
+		gs = &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true, StartupComplete: true}
 		Expect(pfs.checkIfNodeIsDoingStartup(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.startupInProgress).Should(BeFalse())
 	})
@@ -266,7 +266,7 @@ var _ = Describe("podfacts", func() {
 			},
 		}
 		pfs := MakePodFacts(vdbRec, fpr)
-		gs := &GatherState{VerticaPIDRunning: true}
+		gs := &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true}
 		pf := &PodFact{name: pn, isPodRunning: true, dbExists: true}
 		Expect(pfs.checkForSimpleGatherStateMapping(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pfs.checkNodeStatus(ctx, vdb, pf, gs)).Should(Succeed())

commit 9bec17cd19dfc76cafb433a415260218c8f94dcf

Fix e2e test stability (#761)
This is a continuation of issue #750. In summary, we're encountering
intermittent test failures. Upon deleting the pod in admintools
deployments, we observed that the Vertica process shows up in the `ps`
output. A new pod should not have Vertica already running because the
operator should be triggering that. Although this doesn't impact the
operator's functionality, it disrupts end-to-end tests for upgrades, as
they rely on the operator's sequential actions.

I encountered another instance of this issue while investigating with
the debug code introduced in #750. It's evident from the `ps` output
that the Vertica process belongs to a previous instance of the pod. One
clear indication is that the host IP provided to the Vertica process
doesn't match the pod's IP. Therefore, I've implemented this check to
address the test failure.

pkg/controllers/vdb/podfacts.go
@@ -200,8 +200,6 @@ type GatherState struct {
 	DBExists               bool            `json:"dbExists"`
 	VerticaPIDRunning      bool            `json:"verticaPIDRunning"`
 	VerticaProcess         string          `json:"verticaProcess"`
-	SpreadPIDRunning       bool            `json:"spreadPIDRunning"`
-	SpreadProcess          string          `json:"spreadProcess"`
 	UpNode                 bool            `json:"upNode"`
 	StartupComplete        bool            `json:"startupComplete"`
 	Compat21NodeName       string          `json:"compat21NodeName"`
@@ -439,7 +437,7 @@ func (p *PodFacts) genGatherScript(vdb *vapi.VerticaDB, pf *PodFact) string {
 	script := strings.Builder{}
 	script.WriteString(p.genGatherScriptBase(vdb, pf))
 	if pf.execContainerName == names.ServerContainer {
-		script.WriteString(p.genGatherScriptForVerticaPIDCollection())
+		script.WriteString(p.genGatherScriptForVerticaPIDCollection(pf))
 	}
 	return script.String()
 }
@@ -512,20 +510,24 @@ func (p *PodFacts) genGatherScriptBase(vdb *vapi.VerticaDB, pf *PodFact) string 
 	))
 }
 
-func (p *PodFacts) genGatherScriptForVerticaPIDCollection() string {
+func (p *PodFacts) genGatherScriptForVerticaPIDCollection(pf *PodFact) string {
 	// This is only be run when vertica process is running in the container we
 	// are doing the gather. And this is only true if we aren't running the nma
 	// in a separate container.
-	return dedent.Dedent(`
+	//
+	// The pgrep command is used to search for the Vertica process with the '-h
+	// <podIP>' parameter set. This is necessary because in some cases, when a
+	// pod is recreated, it may temporarily show the Vertica process from its
+	// previous incarnation. Since the pod's IP changes in such scenarios, using
+	// the '-h <podIP>' parameter ensures that we identify the Vertica process
+	// that started after the pod was created, adding an extra layer of
+	// fail-safe.
+	return dedent.Dedent(fmt.Sprintf(`
 		echo -n 'verticaPIDRunning: '
-		[[ $(pgrep -f "^.*vertica\s-D") ]] && echo true || echo false
+		[[ $(pgrep -f "/opt/vertica/bin/vertica.*-h %s") ]] && echo true || echo false
 		echo -n 'verticaProcess: '
 		pgrep -f "^.*vertica\s-D" -a | tail -1 || echo error
-		echo -n 'spreadPIDRunning: '
-		[[ $(pgrep spread) ]] && echo true || echo false
-		echo -n 'spreadProcess: '
-		pgrep spread -a | tail -1 || echo error
- 	`)
+ 	`, pf.podIP))
 }
 
 // getPathToVerifyCatalogExists will return a suffix of a path that we can check
@@ -713,7 +715,7 @@ func (p *PodFact) setNodeState(gs *GatherState, useVclusterOps bool) {
 	// At one point, we ran a query against the nodes table. But it became
 	// tricker to decipher what query failure meant -- is vertica down or is it
 	// a problem with the query?
-	p.upNode = p.dbExists && gs.VerticaPIDRunning && gs.SpreadPIDRunning
+	p.upNode = p.dbExists && gs.VerticaPIDRunning
 }
 
 // checkDCTableAnnotations will check if the pod has the necessary annotations

pkg/controllers/vdb/podfacts_test.go
@@ -161,7 +161,7 @@ var _ = Describe("podfacts", func() {
 		fpr := &cmds.FakePodRunner{}
 		pfs := MakePodFacts(vdbRec, fpr)
 		pf := &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs := &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true}
+		gs := &GatherState{VerticaPIDRunning: true}
 		Expect(pfs.checkForSimpleGatherStateMapping(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.upNode).Should(BeTrue())
 	})
@@ -179,7 +179,7 @@ var _ = Describe("podfacts", func() {
 		}
 		pfs := MakePodFacts(vdbRec, fpr)
 		pf := &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs := &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true}
+		gs := &GatherState{VerticaPIDRunning: true}
 		Expect(pfs.checkForSimpleGatherStateMapping(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pfs.checkNodeStatus(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.upNode).Should(BeTrue())
@@ -237,17 +237,17 @@ var _ = Describe("podfacts", func() {
 		fpr := &cmds.FakePodRunner{}
 		pfs := MakePodFacts(vdbRec, fpr)
 		pf := &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs := &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true, StartupComplete: false}
+		gs := &GatherState{VerticaPIDRunning: true, StartupComplete: false}
 		Expect(pfs.checkIfNodeIsDoingStartup(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.startupInProgress).Should(BeTrue())
 
 		pf = &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs = &GatherState{VerticaPIDRunning: false, SpreadPIDRunning: false, StartupComplete: true}
+		gs = &GatherState{VerticaPIDRunning: false, StartupComplete: true}
 		Expect(pfs.checkIfNodeIsDoingStartup(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.startupInProgress).Should(BeFalse())
 
 		pf = &PodFact{name: pn, isPodRunning: true, dbExists: true}
-		gs = &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true, StartupComplete: true}
+		gs = &GatherState{VerticaPIDRunning: true, StartupComplete: true}
 		Expect(pfs.checkIfNodeIsDoingStartup(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pf.startupInProgress).Should(BeFalse())
 	})
@@ -266,7 +266,7 @@ var _ = Describe("podfacts", func() {
 			},
 		}
 		pfs := MakePodFacts(vdbRec, fpr)
-		gs := &GatherState{VerticaPIDRunning: true, SpreadPIDRunning: true}
+		gs := &GatherState{VerticaPIDRunning: true}
 		pf := &PodFact{name: pn, isPodRunning: true, dbExists: true}
 		Expect(pfs.checkForSimpleGatherStateMapping(ctx, vdb, pf, gs)).Should(Succeed())
 		Expect(pfs.checkNodeStatus(ctx, vdb, pf, gs)).Should(Succeed())