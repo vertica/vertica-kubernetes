931e8bfd3c1d4ad223075e193a3d066b6403ada7
Matt Spilchen <mspilchen@opentext.com>
2024-04-29 09:19:10 -0300

    Decouple sc name from sts name

--- pkg/iter/sc_finder.go
+++ pkg/iter/sc_finder.go
@@ -127,7 +127,13 @@ func (m *SubclusterFinder) FindSubclusters(ctx context.Context, flags FindFlags,
 		// indication the subcluster is being removed.
 		for i := range missingSts.Items {
 			scName := missingSts.Items[i].Labels[vmeta.SubclusterNameLabel]
-			subclusters = append(subclusters, &vapi.Subcluster{Name: scName, Size: 0})
+			subclusters = append(subclusters, &vapi.Subcluster{
+				Name: scName,
+				Size: 0,
+				Annotations: map[string]string{
+					vmeta.StsNameOverrideAnnotation: missingSts.Items[i].ObjectMeta.Name,
+				},
+			})
 		}
 	}
 
