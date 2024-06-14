3579b6c5a6e3effe3a19a601c9ca08a7b9f95f60
chinhtranvan <47764275+chinhtranvan@users.noreply.github.com>
2024-06-14 10:58:23 -0500

    Add reconciler to promote the sandbox to the main cluster (#828)
    This PR build a reconciler so that during the upgrade we can just
    promote the sandbox after all of the data has been replicated to it.
    
    ---------
    
    Co-authored-by: cchen-vertica <cai.chen@microfocus.com>

---
+++ pkg/vadmin/promote_sandbox_to_main_vc_test.go
@@ -0,0 +1,61 @@
+/*
+ (c) Copyright [2021-2024] Open Text.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package vadmin
+
+import (
+	"context"
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	vops "github.com/vertica/vcluster/vclusterops"
+	"github.com/vertica/vertica-kubernetes/pkg/vadmin/opts/promotesandboxtomain"
+)
+
+// mock version of VPromoteSandboxToMain() that is invoked inside VClusterOps.PromoteSandboxToMain()
+func (m *MockVClusterOps) VPromoteSandboxToMain(options *vops.VPromoteSandboxToMainOptions) error {
+	// verify common options
+	err := m.VerifyCommonOptions(&options.DatabaseOptions)
+	if err != nil {
+		return err
+	}
+
+	// verify hosts and eon mode
+	err = m.VerifyInitiatorIPAndEonMode(&options.DatabaseOptions)
+	if err != nil {
+		return err
+	}
+
+	// verify basic options
+	if options.SandboxName != TestSandboxName {
+		return fmt.Errorf("failed to verify sandbox name")
+	}
+
+	return nil
+}
+
+var _ = Describe("promote_sandbox_to_main_vc", func() {
+	ctx := context.Background()
+
+	It("should call vclusterOps library with promote_sandbox_to_main task", func() {
+		dispatcher := mockVClusterOpsDispatcher()
+		dispatcher.VDB.Spec.DBName = TestDBName
+		Ω(dispatcher.PromoteSandboxToMain(ctx,
+			promotesandboxtomain.WithInitiator(TestInitiatorIP),
+			promotesandboxtomain.WithSandbox(TestSandboxName),
+		)).Should(Succeed())
+	})
+})
