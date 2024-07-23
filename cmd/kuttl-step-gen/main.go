8894aa509e888861171907bb9eab37159f006b14
cchen-vertica <cai.chen@microfocus.com>
2024-07-23 10:52:08 -0500

    resolved conflicts

--- cmd/kuttl-step-gen/main.go
+++ cmd/kuttl-step-gen/main.go
@@ -31,8 +31,6 @@ import (
 
 const configPath = "/tmp/local-soak.cfg"
 
-const configDir = "/tmp"
-
 func setupLog() logr.Logger {
 	cfg := zap.Config{
 		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
