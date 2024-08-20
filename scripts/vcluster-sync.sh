04c8e1dcbc230d539f2ebccc9a961c22040deb78
hyang <hyang3@opentext.com>
2024-08-20 09:22:06 +0000

    remove vnext from trigger

--- scripts/vcluster-sync.sh
+++ scripts/vcluster-sync.sh
@@ -6,17 +6,6 @@ GITHUB_CRED=$3
 GITHUB_OWNER=vertica
 GITHUB_REPO=vcluster
 
-# THIS IS ANOTHER WAY TO CALL EVENT, LEAVE IT FOR REF FOR NOW
-# # Trigger the GitHub Vcluster workflow dispatch event
-# curl -X POST -v \
-#  -H "Accept: application/vnd.github+json" \
-#  -H "Authorization: Bearer $GITHUB_CRED" \
-#  -H "X-GitHub-Api-Version: 2022-11-28" \
-#  --fail \
-#  https://api.github.com/repos/HaoYang0000/test_vcluster_repo/actions/workflows/merge_branch.yaml/dispatches \
-#  -d '{  "ref":"master", "inputs":{ "source_branch":"$SOURCE_BRANCH", "target_branch":"$TARGET_BRANCH"} }'
-
-
 curl -L -v \
   -X POST \
   -H "Accept: application/vnd.github+json" \
