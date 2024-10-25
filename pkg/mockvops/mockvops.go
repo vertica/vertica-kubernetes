e99e75ecf04da981874d3f46682de94c1135b755
Hao Yamg <hyang3@opentext.com>
2024-10-25 06:12:09 +0000

    add interface and mock methods

--- pkg/mockvops/mockvops.go
+++ pkg/mockvops/mockvops.go
@@ -69,6 +69,9 @@ func (*MockVClusterOps) VStartNodes(_ *vclusterops.VStartNodesOptions) error {
 func (*MockVClusterOps) VStopDatabase(_ *vclusterops.VStopDatabaseOptions) error {
 	return nil
 }
+func (*MockVClusterOps) VStopSubcluster(_ *vclusterops.VStopSubclusterOptions) error {
+	return nil
+}
 func (*MockVClusterOps) VInstallPackages(_ *vclusterops.VInstallPackagesOptions) (*vclusterops.InstallPackageStatus, error) {
 	return nil, nil
 }
