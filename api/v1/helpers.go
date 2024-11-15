c6c15f8deea52a2c537817b72b09205d6dfec9a1
qindotguan <qindotguan@gmail.com>
2024-11-15 14:20:54 -0500

    update client proxy default image

--- api/v1/helpers.go
+++ api/v1/helpers.go
@@ -132,7 +132,7 @@ func MakeVDB() *VerticaDB {
 					Size:        3,
 					ServiceType: corev1.ServiceTypeClusterIP,
 					Type:        PrimarySubcluster,
-					Proxy:       Proxy{Image: "vertica-client-proxy:latest"},
+					Proxy:       Proxy{Image: "opentext/client-proxy:latest"},
 				},
 			},
 		},
