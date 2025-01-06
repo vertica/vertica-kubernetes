6cb3e97d724e10207e8607c803bcdd73a59b4cee
Cai Chen <cai.chen@microfocus.com>
2025-01-06 20:57:23 +0000

    test

--- scripts/setup-minio.sh
+++ scripts/setup-minio.sh
@@ -58,7 +58,7 @@ set -o xtrace
 
 # First setup the operator
 kubectl krew update
-kubectl krew install --manifest-url https://raw.githubusercontent.com/kubernetes-sigs/krew-index/d1817869b86fd040a923682b1392bdb232947bf5/plugins/minio.yaml
+echo "minio" | kubectl krew install --manifest-url https://raw.githubusercontent.com/kubernetes-sigs/krew-index/d1817869b86fd040a923682b1392bdb232947bf5/plugins/minio.yaml
 # If these images ever change, they must be updated in tests/external-images-s3-ci.txt
 kubectl minio init --image minio/operator:v4.5.7
 
