c5f36c11ec72ed6cb24f5bfb43b9bfaa831d49df
gitlab-cicd <cchen8@opentext.com>
2025-01-08 17:01:17 +0000

    CICD for commit 2d88fbe2660c9187ae4041645e704f2f16b02a64

--- local-libs/vcluster/commands/cmd_create_connection.go
+++ local-libs/vcluster/commands/cmd_create_connection.go
@@ -16,8 +16,9 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
-	"strings"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/vertica/vcluster/vclusterops"
@@ -25,10 +26,8 @@ import (
 )
 
 const (
-	dotYaml           = ".yaml"
-	dotYml            = ".yml"
-	hostWarningMsgOne = "some hosts are invalid: "
-	hostWarningMsgTwo = "We removed those invalid hosts from the connection file."
+	dotYaml = ".yaml"
+	dotYml  = ".yml"
 )
 
 /* CmdCreateConnection
@@ -36,16 +35,15 @@ const (
  * Implements ClusterCommand interface
  */
 type CmdCreateConnection struct {
-	checkConnection   bool
-	connectionOptions *vclusterops.DatabaseOptions
+	connectionOptions *vclusterops.VReplicationDatabaseOptions
 	CmdBase
 }
 
 func makeCmdCreateConnection() *cobra.Command {
 	newCmd := &CmdCreateConnection{}
-	options := vclusterops.DatabaseOptionsFactory()
-	newCmd.connectionOptions = &options
-	newCmd.connectionOptions.Password = new(string)
+	opt := vclusterops.VReplicationDatabaseFactory()
+	newCmd.connectionOptions = &opt
+	opt.TargetDB.Password = new(string)
 
 	cmd := makeBasicCobraCmd(
 		newCmd,
@@ -56,7 +54,7 @@ The generated connection file should be used with the replication command.
 
 Examples:
   # create the connection file to /tmp/vertica_connection.yaml
-  vcluster connection create --db-name platform_test_db --hosts 10.20.30.43 --db-user \ 
+  vcluster create_connection --db-name platform_test_db --hosts 10.20.30.43 --db-user \ 
     dkr_dbadmin --password-file /tmp/password.txt --conn /tmp/vertica_connection.yaml
 `,
 		[]string{connFlag},
@@ -64,32 +62,33 @@ Examples:
 
 	// local flags
 	newCmd.setLocalFlags(cmd)
-	cmd.AddCommand(makeCheckConnectionCmd())
-	markFlagsRequired(cmd, dbNameFlag, hostsFlag, passwordFileFlag, connFlag)
+
+	markFlagsRequired(cmd, dbNameFlag, hostsFlag, connFlag)
 	return cmd
 }
 
 // setLocalFlags will set the local flags the command has
 func (c *CmdCreateConnection) setLocalFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVar(
-		&c.connectionOptions.DBName,
+		&c.connectionOptions.TargetDB.DBName,
 		dbNameFlag,
 		"",
 		"The name of the database. You should only use this option if you want to override the database name in your configuration file.",
 	)
 	cmd.Flags().StringSliceVar(
-		&c.connectionOptions.RawHosts,
+		&c.connectionOptions.TargetDB.Hosts,
 		hostsFlag,
 		[]string{},
 		"A comma-separated list of hosts in database.")
 	cmd.Flags().StringVar(
-		&c.connectionOptions.UserName,
+		&c.connectionOptions.TargetDB.UserName,
 		dbUserFlag,
 		"",
 		"The name of the user in the target database.",
 	)
+	//  password flags
 	cmd.Flags().StringVar(
-		&c.passwordFile,
+		c.connectionOptions.TargetDB.Password,
 		passwordFileFlag,
 		"",
 		"The absolute path to a file containing the password to the target database.",
@@ -99,95 +98,44 @@ func (c *CmdCreateConnection) setLocalFlags(cmd *cobra.Command) {
 		connFlag,
 		"",
 		"The absolute path to the connection file in yaml format.")
-	cmd.Flags().BoolVar(
-		&c.checkConnection,
-		"check-connection",
-		false,
-		"validate user inputs before creating the connection file",
-	)
 	markFlagsFileName(cmd, map[string][]string{connFlag: {"yaml"}})
 }
 
 func (c *CmdCreateConnection) Parse(inputArgv []string, logger vlog.Printer) error {
 	c.argv = inputArgv
 	logger.LogMaskedArgParse(c.argv)
-	err := c.setDBPassword(c.connectionOptions)
-	if err != nil {
-		return err
-	}
-	err = c.validateParse(logger)
-	return err
+	return c.validateParse(logger)
 }
 
 func (c *CmdCreateConnection) validateParse(logger vlog.Printer) error {
-	connFile := globals.connFile
-	err := validateYamlFilePath(connFile, logger)
-	if err != nil {
-		return err
+	if !filepath.IsAbs(globals.connFile) {
+		filePathError := errors.New(
+			"Invalid connection file path: " + globals.connFile + ". The connection file path must be absolute.")
+		logger.Error(filePathError, "Connection file path error:")
+		return filePathError
 	}
-	return c.ValidateParseBaseOptions(c.connectionOptions)
+	ext := filepath.Ext(globals.connFile)
+	if ext != dotYaml && ext != dotYml {
+		fileTypeError := errors.New("Invalid file type: " + ext + ". Only .yaml or .yml is allowed.")
+		logger.Error(fileTypeError, "Connection file type error:")
+		return fileTypeError
+	}
+	return c.ValidateParseBaseOptions(&c.connectionOptions.DatabaseOptions)
 }
 
 func (c *CmdCreateConnection) Run(vcc vclusterops.ClusterCommands) error {
 	vcc.LogInfo("Called method Run()")
-	if c.checkConnection {
-		invalidHosts, err := c.verifyAndUpdateConnectionOptions(vcc)
-		if err != nil {
-			vcc.DisplayError(converErrorMessage(err, vcc.GetLog()))
-			return nil
-		}
-		if len(invalidHosts) > 0 {
-			vcc.DisplayWarning(hostWarningMsgOne + strings.Join(invalidHosts, ",") + " " + hostWarningMsgTwo)
-		}
-		vcc.DisplayInfo("Successfully verified connection parameters")
-	} else {
-		c.connectionOptions.Hosts = c.connectionOptions.RawHosts
-	}
+
 	// write target db info to vcluster connection file
-	err := c.writeConn()
+	err := writeConn(c.connectionOptions)
 	if err != nil {
-		vcc.DisplayError("failed to write the connection file: " + err.Error())
-		return nil
+		return fmt.Errorf("failed to write the connection file: %w", err)
 	}
 	vcc.DisplayInfo("Successfully wrote the connection file in %s", globals.connFile)
-	return err
+	return nil
 }
 
 // SetDatabaseOptions will assign a vclusterops.DatabaseOptions instance
 func (c *CmdCreateConnection) SetDatabaseOptions(opt *vclusterops.DatabaseOptions) {
-	c.connectionOptions.LogPath = opt.LogPath
-}
-
-func (c *CmdCreateConnection) verifyAndUpdateConnectionOptions(vcc vclusterops.ClusterCommands) ([]string, error) {
-	fetchNodeDetailsOptions := vclusterops.VFetchNodesDetailsOptionsFactory()
-
-	fetchNodeDetailsOptions.DatabaseOptions = *c.connectionOptions
-	fetchNodeDetailsOptions.LogPath = c.connectionOptions.LogPath
-	validHosts, invalidHosts, returnErr := fetchNodeDetails(vcc, &fetchNodeDetailsOptions)
-	if len(validHosts) > 0 {
-		c.connectionOptions.Hosts = validHosts
-		return invalidHosts, nil
-	}
-	return invalidHosts, returnErr
-}
-
-// writeConn will save instructions for connecting to a database into a connection file.
-func (c *CmdCreateConnection) writeConn() error {
-	if globals.connFile == "" {
-		return fmt.Errorf("conn path is empty")
-	}
-	dbConn := c.readTargetDBToDBConn()
-	// write a connection file with the given target database info from create_connection
-	err := dbConn.write(globals.connFile)
-	return err
-}
-
-// readTargetDBToDBConn converts target database to DatabaseConnection
-func (c *CmdCreateConnection) readTargetDBToDBConn() DatabaseConnection {
-	targetDBconn := MakeTargetDatabaseConn()
-	targetDBconn.TargetDBName = c.connectionOptions.DBName
-	targetDBconn.TargetHosts = c.connectionOptions.Hosts
-	targetDBconn.TargetPasswordFile = c.passwordFile
-	targetDBconn.TargetDBUser = c.connectionOptions.UserName
-	return targetDBconn
+	c.connectionOptions.DatabaseOptions = *opt
 }
--- local-libs/vcluster/vclusterops/vcluster_version.go
+++ local-libs/vcluster/vclusterops/vcluster_version.go
@@ -13,6 +13,7 @@
  limitations under the License.
 */
 
+// test 22
 package vclusterops
 
 import (
