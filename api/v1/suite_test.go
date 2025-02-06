000724de0ab1f8b311b506daf2add339ec5e1ce2
Hao Yamg <hyang3@opentext.com>
2025-02-06 09:00:18 +0000

    move test target to v1

--- api/v1/suite_test.go
+++ api/v1/suite_test.go
@@ -98,6 +98,9 @@ var _ = BeforeSuite(func() {
 	err = (&VerticaDB{}).SetupWebhookWithManager(mgr)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = (&VerticaAutoscaler{}).SetupWebhookWithManager(mgr)
+	Expect(err).NotTo(HaveOccurred())
+
 	//+kubebuilder:scaffold:webhook
 
 	go func() {
