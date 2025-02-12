4d6066506b9e8c537d2c00499bb0f107d3d435d5
gitlab-cicd <cchen8@opentext.com>
2025-02-12 19:19:46 +0000

    CICD for commit bcf1884fb018a62e9a515dd3ad3b3d85066e5cf9

--- local-libs/vcluster/vclusterops/cmd_type.go
+++ local-libs/vcluster/vclusterops/cmd_type.go
@@ -45,6 +45,7 @@ const (
 	PollSubclusterStateCmd
 	UpgradeLicenseCmd
 	ClusterHealthCmd
+	RotateNMACertsCmd
 )
 
 var cmdStringMap = map[CmdType]string{
@@ -88,6 +89,7 @@ var cmdStringMap = map[CmdType]string{
 	PollSubclusterStateCmd:       "poll_subcluster_state",
 	UpgradeLicenseCmd:            "upgrade_license",
 	ClusterHealthCmd:             "cluster_health",
+	RotateNMACertsCmd:            "rotate_nma_certs",
 }
 
 func (cmd CmdType) CmdString() string {
