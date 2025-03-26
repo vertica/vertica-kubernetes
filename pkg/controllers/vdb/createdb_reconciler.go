65f7b44a6c82f7d7035407d3b71d9cef12d32714
Libo Yu <libo.yu@microfocus.com>
2025-03-26 15:18:46 -0400

    fix unit test

--- pkg/controllers/vdb/createdb_reconciler.go
+++ pkg/controllers/vdb/createdb_reconciler.go
@@ -130,7 +130,17 @@ func (c *CreateDBReconciler) execCmd(ctx context.Context, initiatorPod types.Nam
 	if res, err := c.Dispatcher.CreateDB(ctx, opts...); verrors.IsReconcileAborted(res, err) {
 		return res, err
 	}
-	clusterPodRunner := c.PRunner.(*cmds.ClusterPodRunner)
+	passwd := ""
+	clusterPodRunner, ok := c.PRunner.(*cmds.ClusterPodRunner)
+	if ok {
+		passwd = clusterPodRunner.VerticaSUPassword
+	} else {
+		fakePodRunner, ok2 := c.PRunner.(*cmds.FakePodRunner)
+		if ok2 {
+			passwd = fakePodRunner.VerticaSUPassword
+		}
+	}
+
 	if c.Vdb.IsCertRotationEnabled() {
 		cmd := []string{
 			"--password", clusterPodRunner.VerticaSUPassword, "-f", PostDBCreateSQLFileVclusterOps,
