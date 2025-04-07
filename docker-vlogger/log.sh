1c9d954392399ede12e560ac51706ca4b95dddb1
Hao Yamg <hyang3@opentext.com>
2025-04-07 12:51:03 +0000

    update script

--- docker-vlogger/log.sh
+++ docker-vlogger/log.sh
@@ -5,9 +5,9 @@ FN=$DBPATH/v_*_catalog/vertica.log
 
 if [[ "$LOG_LEVEL" == "WARNING" ]]; then
    until [ -f $FN ]; do sleep 5; done; tail -n 1 -F $FN | grep WARNING
-elif [[ "$LOG_LEVEL" == "INFO" ]]; then
-   until [ -f $FN ]; do sleep 5; done; tail -n 1 -F $FN | grep INFO
+elif [[ "$LOG_LEVEL" == "ERROR" ]]; then
+   until [ -f $FN ]; do sleep 5; done; tail -n 1 -F $FN | grep ERROR
 else
-   FN=$DBPATH/v_*_catalog/vertica.log; until [ -f $FN ]; do sleep 5; done; tail -n 1 -F $FN
+   until [ -f $FN ]; do sleep 5; done; tail -n 1 -F $FN
 fi
 
