e272d86b294a30d0b619d11b2edfd7f77a411218
Libo Yu <libo.yu@microfocus.com>
2025-04-16 15:52:22 -0400

    add e2e test which fails currently

---
+++ tests/e2e-leg-13/nma-https-cert-rotate-with-pwd/vsql_verify.sh
@@ -0,0 +1,21 @@
+K8S_TLS_ENABLED=$(kubectl exec -n $NAMESPACE v-tls-certs-sc1-0 -c server -- vsql -U dbadmin -w superuser -tAc "SELECT is_auth_enabled FROM client_auth where auth_name='k8s_tls_builtin_auth';")
+if [[ $K8S_TLS_ENABLED != "True" ]]; then
+    echo "ERROR: k8s_tls_builtin_auth not enabled"
+    exit 1
+fi
+CERT_NAME=$(kubectl exec -n $NAMESPACE v-tls-certs-sc1-0 -c server -- vsql -U dbadmin -w superuser -tAc "select certificate from tls_configurations where name='https' and owner='dbadmin';")
+if [[ $CERT_NAME != "https_cert_1" ]]; then
+    echo "ERROR: cert https_cert_1 not found"
+    exit 1
+fi
+CA_CERT_NAME=$(kubectl exec -n $NAMESPACE v-tls-certs-sc1-0 -c server -- vsql -U dbadmin -w superuser -tAc "select ca_certificate from tls_configurations where name='https' and owner='dbadmin';")
+if [[ $CA_CERT_NAME != "https_ca_cert_1" ]]; then
+    echo "ERROR: cert https_ca_cert_1 not found"
+    exit 1
+fi
+KEY_NAME=$(kubectl exec -n $NAMESPACE v-tls-certs-sc1-0 -c server -- vsql -U dbadmin -w superuser -tAc "select name FROM cryptographic_keys WHERE secret_name='custom-cert';")
+if [[ $KEY_NAME != "https_key_1" ]]; then
+    echo "ERROR: key https_key_1 not found"
+    exit 1
+fi
+exit 0
