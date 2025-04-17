d13c09d1118cc7892ee0069f3628f9b208fda0a5
Libo Yu <libo.yu@microfocus.com>
2025-04-17 13:38:49 -0400

    VERIFY_CA works for client server TLS

--- tests/e2e-leg-9/nma-user-cert-config/vsql_verify_nma_client_tls.sh
+++ tests/e2e-leg-9/nma-user-cert-config/vsql_verify_nma_client_tls.sh
@@ -4,8 +4,8 @@ if [[ $SERVER_TLS_SECRET_NAME != "client-cert" ]]; then
     exit 1
 fi
 SERVER_TLS_MODE=$(kubectl exec -n $NAMESPACE v-tls-certs-sc1-0 -c server -- vsql -tAc "select mode FROM tls_configurations WHERE name='server';")
-if [[ $SERVER_TLS_MODE != "TRY_VERIFY" ]]; then
-    echo "ERROR: server TLS mode is not TRY_VERIFY"
+if [[ $SERVER_TLS_MODE != "VERIFY_CA" ]]; then
+    echo "ERROR: server TLS mode is not VERIFY_CA"
     exit 1
 fi
 exit 0
