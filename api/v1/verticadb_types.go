5f2d78ff05aaf1aca90558a64932d0a8338397fd
Libo Yu <libo.yu@microfocus.com>
2025-04-22 20:37:53 -0400

    fix scorecard test

--- api/v1/verticadb_types.go
+++ api/v1/verticadb_types.go
@@ -301,7 +301,7 @@ type VerticaDBSpec struct {
 	// service you are storing.
 	ClientServerTLSSecret string `json:"clientServerTLSSecret,omitempty"`
 
-	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:select:DISABLE"
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:select:DISABLE",
 	// "urn:alm:descriptor:com.tectonic.ui:select:ENABLE","urn:alm:descriptor:com.tectonic.ui:select:TRY_VERIFY",
 	// "urn:alm:descriptor:com.tectonic.ui:select:VERIFY_CA"}
 	// +kubebuilder:default:=VERIFY_CA
