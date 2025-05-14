743df248455efdfc32a7a399f6198cd18487b440
qindotguan <qindotguan@gmail.com>
2025-05-14 06:57:26 -0400

    get subcluster status type from podfacts

--- pkg/controllers/vdb/status_reconciler.go
+++ pkg/controllers/vdb/status_reconciler.go
@@ -190,18 +190,25 @@ func (s *StatusReconciler) calculateSubclusterStatusType(sc *vapi.Subcluster) st
 		return ""
 	}
 
+	pn := names.GenPodName(s.Vdb, sc, 0)
+	pf, ok := s.PFacts.Detail[pn]
+	if !ok {
+		s.Log.Info("No pods found in the subcluster to calculate its status type",
+			"subcluster", sc.Name)
+		return ""
+	}
+
 	// if in a sandbox, set subcluster type according to the sandbox status
-	sandboxStatus := s.Vdb.GetSandboxStatus(s.PFacts.SandboxName)
-	isSandbox := sandboxStatus != nil && sandboxStatus.Name != vapi.MainCluster
+	isSandbox := pf.GetSandbox() != ""
 	if isSandbox {
-		if sc.IsSandboxPrimary(s.Vdb) {
+		if pf.GetIsPrimary() {
 			return vapi.SandboxPrimarySubcluster
 		} else {
 			return vapi.SandboxSecondarySubcluster
 		}
 	}
 
-	if sc.IsPrimary(s.Vdb) {
+	if pf.GetIsPrimary() {
 		return vapi.PrimarySubcluster
 	}
 
