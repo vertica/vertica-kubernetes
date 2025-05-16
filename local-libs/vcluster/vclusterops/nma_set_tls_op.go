8dafc516750f82c4f3be7ceee4488e408e37815a
Cai Chen <cchen8@opentext.com>
2025-05-16 14:23:53 +0000

    update vertica images

--- local-libs/vcluster/vclusterops/nma_set_tls_op.go
+++ local-libs/vcluster/vclusterops/nma_set_tls_op.go
@@ -74,7 +74,7 @@ func (op *nmaSetTLSOp) setupRequestBody(
 	setConfigData.TLSConfigName = configName
 	setConfigData.TLSNamespace = configMap[TLSSecretManagerKeyNamespace]
 	setConfigData.TLSSecretName = configMap[TLSSecretManagerKeySecretName]
-	setConfigData.TLSMode = genNMACompatibleTLSMode(configMap[TLSSecretManagerKeyTLSMode])
+	setConfigData.TLSMode = configMap[TLSSecretManagerKeyTLSMode]
 	setConfigData.TLSSecretManager = configMap[TLSSecretManagerKeySecretManager]
 	setConfigData.AWSRegion = configMap[TLSSecretManagerKeyAWSRegion]
 	setConfigData.AWSSecretVersionID = configMap[TLSSecretManagerKeyAWSSecretVersionID]
@@ -137,9 +137,4 @@ func (op *nmaSetTLSOp) processResult(_ *opEngineExecContext) error {
 	}
 
 	return allErrs
-}
-
-func genNMACompatibleTLSMode(tlsMode string) string {
-	m := regexp.MustCompile(`_`)
-	return m.ReplaceAllString(tlsMode, "-")
 }
