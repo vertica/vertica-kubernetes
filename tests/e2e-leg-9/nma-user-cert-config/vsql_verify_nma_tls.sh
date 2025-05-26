da68181e133f58e7587e5051c36b95f4ee7b73b8
Libo Yu <libo.yu@microfocus.com>
2025-05-25 20:26:08 -0400

    change leg 9 nma-user-cert-config verify script tls mode name

--- tests/e2e-leg-9/nma-user-cert-config/vsql_verify_nma_tls.sh
+++ tests/e2e-leg-9/nma-user-cert-config/vsql_verify_nma_tls.sh
@@ -4,8 +4,8 @@ if [[ $NMA_TLS_SECRET_NAME != "nma-cert" ]]; then
     exit 1
 fi
 HTTPS_TLS_MODE=$(kubectl exec -n $NAMESPACE v-tls-certs-sc1-0 -c server -- vsql -tAc "select mode FROM tls_configurations WHERE name='https';")
-if [[ $HTTPS_TLS_MODE != "TRY_VERIFY" ]]; then
-    echo "ERROR: HTTPS TLS mode is not TRY_VERIFY"
+if [[ $HTTPS_TLS_MODE != "VERIFY_CA" ]]; then
+    echo "ERROR: HTTPS TLS mode is not VERIFY_CA"
     exit 1
 fi
 exit 0
