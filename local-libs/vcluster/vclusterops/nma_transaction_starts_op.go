4a1d44b9c146d1e45aec989b43cba5063fea0269
gitlab-cicd <cchen8@opentext.com>
2025-06-13 13:43:36 +0000

    CICD for commit cfee025673b11f5fba47912cc18456bf7abda350

--- local-libs/vcluster/vclusterops/nma_transaction_starts_op.go
+++ local-libs/vcluster/vclusterops/nma_transaction_starts_op.go
@@ -140,7 +140,8 @@ func (op *nmaTransactionStartsOp) processResult(execContext *opEngineExecContext
 			var transactionStartList []dcTransactionStarts
 			err := op.parseAndCheckResponse(host, result.content, &transactionStartList)
 			if err != nil {
-				return errors.Join(allErrs, err)
+				_ = errors.Join(allErrs, err)
+				continue
 			}
 			// we only need result from one host
 			execContext.dcTransactionStarts = &transactionStartList
