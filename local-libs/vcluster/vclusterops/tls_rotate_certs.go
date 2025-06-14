6bbed209133ab68aa7630a5a31c29580f768153f
gitlab-cicd <cchen8@opentext.com>
2025-06-14 00:47:39 +0000

    CICD for commit b4449f1238f4199fe8317df6256faa1c57054bac

--- local-libs/vcluster/vclusterops/tls_rotate_certs.go
+++ local-libs/vcluster/vclusterops/tls_rotate_certs.go
@@ -200,7 +200,7 @@ func (vcc VClusterCommands) VRotateTLSCerts(options *VRotateTLSCertsOptions) err
 	// the rotation operations need one UP host from each sandbox + main cluster.  the
 	// polling operations should poll each previously UP host in the entire cluster
 	// for restart.
-	upHosts, initiatorHosts, hostsToSandboxes, err := options.getVDBInfo(&vdb)
+	upHosts, initiatorHosts, mainClusterHosts, hostsToSandboxes, err := options.getVDBInfo(&vdb)
 	if err != nil {
 		return err
 	}
@@ -211,7 +211,8 @@ func (vcc VClusterCommands) VRotateTLSCerts(options *VRotateTLSCertsOptions) err
 	expectedTLSConfigInfo := &tlsConfigInfo{}
 
 	// produce rotation instructions
-	instructions, err := vcc.produceRotateTLSCertsInstructions(options, initiatorHosts, hostsToSandboxes, expectedTLSConfigInfo)
+	instructions, err := vcc.produceRotateTLSCertsInstructions(options, initiatorHosts, mainClusterHosts, hostsToSandboxes,
+		expectedTLSConfigInfo)
 	if err != nil {
 		return fmt.Errorf("failed to produce rotate HTTPS certs instructions, %w", err)
 	}
@@ -258,7 +259,7 @@ func (vcc VClusterCommands) VRotateTLSCerts(options *VRotateTLSCertsOptions) err
 }
 
 func (opt *VRotateTLSCertsOptions) getVDBInfo(
-	vdb *VCoordinationDatabase) (upHosts, initiatorHosts []string, hostsToSandboxes map[string]string, err error) {
+	vdb *VCoordinationDatabase) (upHosts, initiatorHosts, mainClusterHosts []string, hostsToSandboxes map[string]string, err error) {
 	upHosts = vdb.filterUpHostList(opt.Hosts)
 	hostsToSandboxes = vdb.getHostToSandboxMap()
 	// avoid mutating backing array of vdb.AllSandboxes
@@ -266,6 +267,14 @@ func (opt *VRotateTLSCertsOptions) getVDBInfo(
 	copy(sandboxes, vdb.AllSandboxes)
 	sandboxes = append(sandboxes, "") // add main cluster to sandbox list
 	initiatorHosts, err = getInitiatorsInAllDBGroups(upHosts, sandboxes, hostsToSandboxes)
+	if err != nil {
+		return
+	}
+	mainCluster := []string{""}
+	mainClusterHosts, err = getInitiatorsInAllDBGroups(upHosts, mainCluster, hostsToSandboxes)
+	if len(mainCluster) == 0 {
+		err = fmt.Errorf("failed to find an initiator host for main cluster")
+	}
 	return
 }
 
@@ -276,7 +285,7 @@ func (opt *VRotateTLSCertsOptions) getVDBInfo(
 //   - Rotate the certs (and optionally update TLS mode)
 func (vcc VClusterCommands) produceRotateTLSCertsInstructions(
 	options *VRotateTLSCertsOptions,
-	initiatorHosts []string,
+	initiatorHosts, mainClusterHosts []string,
 	hostsToSandboxes map[string]string,
 	expectedTLSConfigInfo *tlsConfigInfo) ([]clusterOp, error) {
 	var instructions []clusterOp
@@ -290,6 +299,11 @@ func (vcc VClusterCommands) produceRotateTLSCertsInstructions(
 		return instructions, err
 	}
 	instructions = append(instructions, &nmaRotateTLSCertsOp)
+	httpsSyncCatalogOp, err := makeHTTPSSyncCatalogOp(mainClusterHosts, true, options.UserName, options.Password, CreateDBSyncCat)
+	if err != nil {
+		return instructions, err
+	}
+	instructions = append(instructions, &httpsSyncCatalogOp)
 	return instructions, nil
 }
 
