5896ddd1119dd572c7fd33aea8993eb013d2a9f5
Libo Yu <libo.yu@microfocus.com>
2025-08-08 16:02:54 -0400

    webhook ready and some improvements

--- pkg/security/tls2_cipher_suites.go
+++
@@ -1,9 +0,0 @@
-package security
-
-var TLS2CipherSuites = map[string]void{
-	"ECDHE-RSA-AES256-GCM-SHA384": {},
-	"ECDHE-RSA-CHACHA20-POLY1305": {},
-	"ECDHE-RSA-AES128-GCM-SHA256": {},
-	"ECDHE-RSA-AES256-SHA":        {},
-	"ECDHE-RSA-AES128-SHA":        {},
-}
