ba66bab0024d063aad87196716b402ff3e7e530f
Qin Guan <qindotguan@gmail.com>
2025-09-22 16:28:12 -0400

    Create minimum RBAC resources for Alloy (#1414)
    This PR creates minimum RBAC resources (serviceaccount, role, role
    binding) for Alloy. New templates added for alloy service-account, role,
    and role binding that contain only needed permissions.
    
    No new resources were created for Loki, as the default is already the
    minimum.

--- scripts/template-helm-chart.sh
+++ scripts/template-helm-chart.sh
@@ -267,3 +267,13 @@ done
 perl -i -0777 -pe 's/^/{{- if and .Values.alloy.enabled (not .Values.alloy.alloy.configMap.create) -}}\n/ if 1 .. 1' $TEMPLATE_DIR/verticadb-operator-alloy-cm.yaml
 perl -i -0777 -pe 's/name: \{\{ include "vdb-op.name" \. \}\}-alloy/name: vdb-op-alloy/' $TEMPLATE_DIR/verticadb-operator-alloy-cm.yaml
 echo "{{- end }}" >> $TEMPLATE_DIR/verticadb-operator-alloy-cm.yaml
+
+# 28. Conditionally create alloy rbac resources
+for f in $TEMPLATE_DIR/verticadb-operator-alloy-sa-sa.yaml \
+  $TEMPLATE_DIR/verticadb-operator-alloy-role-cr.yaml \
+  $TEMPLATE_DIR/verticadb-operator-alloy-role-binding-crb.yaml
+do
+  perl -i -0777 -pe 's/^/{{- if and .Values.alloy.enabled (not .Values.alloy.serviceAccount.create) (eq .Values.alloy.serviceAccount.name "alloy-vertica-sa") -}}\n/ if 1 .. 1' $f
+  perl -i -0777 -pe 's/name: \{\{ include "vdb-op.name" \. \}\}-alloy-sa/name: alloy-vertica-sa/g' $f
+  echo "{{- end }}" >> $f
+done
