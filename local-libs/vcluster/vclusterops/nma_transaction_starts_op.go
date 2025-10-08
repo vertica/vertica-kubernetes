0626628124c4cae1aee5b6884f6a766f9f29db8b
gitlab-cicd <cchen8@opentext.com>
2025-10-08 10:57:21 +0000

    CICD for commit e5f690f992ea0454522dddec5374f352e89072f1

--- local-libs/vcluster/vclusterops/nma_transaction_starts_op.go
+++ local-libs/vcluster/vclusterops/nma_transaction_starts_op.go
@@ -111,6 +111,7 @@ func (op *nmaTransactionStartsOp) setupClusterHTTPRequest(hosts []string) error 
 }
 
 func (op *nmaTransactionStartsOp) prepare(execContext *opEngineExecContext) error {
+	execContext.dispatcher.opBase = op.opBase
 	execContext.dispatcher.setup(op.hosts)
 	// Disable the spinner for this op as the op can be called multiple times.
 	// This way would avoid repetive and confusing information.
