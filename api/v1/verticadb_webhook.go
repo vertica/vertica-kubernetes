632e973fa4cab1d28fca7921a6599a13d79f11bf
Libo Yu <libo.yu@microfocus.com>
2025-10-27 09:18:04 -0400

    address a new pr comment

--- api/v1/verticadb_webhook.go
+++ api/v1/verticadb_webhook.go
@@ -741,7 +741,7 @@ func (v *VerticaDB) hasValidSaveRestorePointConfig(allErrs field.ErrorList) fiel
 }
 
 func (v *VerticaDB) hasNonEmptyLicenseSecret(allErrs field.ErrorList) field.ErrorList {
-	if v.Spec.InitPolicy == CommunalInitPolicyCreate && v.UseVClusterOpsDeployment() && !vmeta.GetAllowCELicense(v.Annotations) {
+	if v.UseVClusterOpsDeployment() && !vmeta.GetAllowCELicense(v.Annotations) {
 		if v.Spec.LicenseSecret == "" {
 			err := field.Invalid(field.NewPath("spec").Child("licenseSecret"),
 				v.Spec.LicenseSecret,
