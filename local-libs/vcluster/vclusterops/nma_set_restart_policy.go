2e3fe5dedc87f3e7abb28da02df1222070353d8b
gitlab-cicd <cchen8@opentext.com>
2025-12-10 17:39:05 -0500

    CICD for commit d435067cd0d202f81141ad7873bc3bd8b3319609

---
+++ local-libs/vcluster/vclusterops/nma_set_restart_policy.go
@@ -0,0 +1,122 @@
+/*
+ (c) Copyright [2023-2024] Open Text.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package vclusterops
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type nmaSetRestartPolicyOp struct {
+	opBase
+	hostRequestBodyMap map[string]string
+	policy             string
+}
+
+type setRestartPolicyRequestData struct {
+	sqlEndpointData
+	Params map[string]any `json:"params"`
+}
+
+func makeNMASetRestartPolicyOp(upHosts []string, userName string,
+	dbName string, password *string,
+	restartPolicy string) (nmaSetRestartPolicyOp, error) {
+	op := nmaSetRestartPolicyOp{}
+	op.hosts = upHosts[:1] // set up the request for one of the up hosts only
+	op.policy = restartPolicy
+
+	// NMA endpoints don't need to differentiate between empty password and no password
+	useDBPassword := password != nil
+	err := ValidateSQLEndpointData(op.name,
+		useDBPassword, userName, password, dbName)
+	if err != nil {
+		return op, err
+	}
+	err = op.setupRequestBody(userName, dbName, useDBPassword, password)
+	return op, err
+}
+
+func (op *nmaSetRestartPolicyOp) setupRequestBody(username, dbName string, useDBPassword bool,
+	password *string) error {
+	op.hostRequestBodyMap = make(map[string]string)
+
+	for _, host := range op.hosts {
+		requestData := setRestartPolicyRequestData{}
+
+		requestData.sqlEndpointData = createSQLEndpointData(username, dbName, useDBPassword, password)
+		requestData.Params = make(map[string]any)
+		requestData.Params["policy"] = op.policy
+
+		dataBytes, err := json.Marshal(requestData)
+		if err != nil {
+			return fmt.Errorf("[%s] fail to marshal request data to JSON string, detail %w", op.name, err)
+		}
+
+		op.hostRequestBodyMap[host] = string(dataBytes)
+	}
+
+	return nil
+}
+
+func (op *nmaSetRestartPolicyOp) setupClusterHTTPRequest(hosts []string) error {
+	for _, host := range hosts {
+		httpRequest := hostHTTPRequest{}
+		httpRequest.Method = PostMethod
+		httpRequest.buildNMAEndpoint("restart-policy/set")
+		httpRequest.RequestData = op.hostRequestBodyMap[host]
+		op.clusterHTTPRequest.RequestCollection[host] = httpRequest
+	}
+
+	return nil
+}
+
+func (op *nmaSetRestartPolicyOp) prepare(execContext *opEngineExecContext) error {
+	execContext.dispatcher.setup(op.hosts)
+	return op.setupClusterHTTPRequest(op.hosts)
+}
+
+func (op *nmaSetRestartPolicyOp) execute(execContext *opEngineExecContext) error {
+	if err := op.runExecute(execContext); err != nil {
+		return err
+	}
+
+	return op.processResult(execContext)
+}
+
+func (op *nmaSetRestartPolicyOp) finalize(_ *opEngineExecContext) error {
+	return nil
+}
+
+func (op *nmaSetRestartPolicyOp) processResult(_ *opEngineExecContext) error {
+	var allErrs error
+	for host, result := range op.clusterHTTPRequest.ResultCollection {
+		// for any passing result, directly return
+		if result.isPassing() {
+			if !strings.Contains(result.content, "Database option set") {
+				return fmt.Errorf("failed to set restart policy on host %s: %v", host, result.content)
+			}
+
+			return nil
+		}
+
+		// record the error in failed results
+		allErrs = errors.Join(allErrs, result.err)
+	}
+
+	return allErrs
+}
---
+++ local-libs/vcluster/vclusterops/set_restart_policy.go
@@ -0,0 +1,129 @@
+/*
+ (c) Copyright [2023-2025] Open Text.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package vclusterops
+
+import (
+	"fmt"
+
+	"github.com/vertica/vcluster/vclusterops/util"
+)
+
+// VSetRestartPolicyOptions set the restart policy options for a database
+type VSetRestartPolicyOptions struct {
+	DatabaseOptions
+	Policy string
+}
+
+func VSetRestartPolicyOptionsFactory() VSetRestartPolicyOptions {
+	options := VSetRestartPolicyOptions{}
+	// set default values to the params
+	options.setDefaultValues()
+	options.Policy = util.DefaultRestartPolicy
+
+	return options
+}
+
+// analyzeOptions verifies the host options for the VSetRestartPolicyOptions struct and
+// returns any error encountered.
+func (options *VSetRestartPolicyOptions) analyzeOptions() error {
+	if len(options.RawHosts) > 0 {
+		hostAddresses, err := util.ResolveRawHostsToAddresses(options.RawHosts, options.IPv6)
+		if err != nil {
+			return err
+		}
+		options.Hosts = hostAddresses
+	}
+
+	if !util.StringInArray(options.Policy, util.RestartPolicyList) {
+		return fmt.Errorf("policy must be one of %v", util.RestartPolicyList)
+	}
+
+	return nil
+}
+
+func (options *VSetRestartPolicyOptions) validateParseOptions() error {
+	if options.DBName == "" {
+		return fmt.Errorf("database name must be provided")
+	}
+
+	err := util.ValidateDBName(options.DBName)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (options *VSetRestartPolicyOptions) validateAnalyzeOptions() error {
+	err := options.validateParseOptions()
+	if err != nil {
+		return err
+	}
+
+	return options.analyzeOptions()
+}
+
+func (vcc VClusterCommands) VSetRestartPolicy(options *VSetRestartPolicyOptions) error {
+	/*
+	 *   - Produce Instructions
+	 *   - Create a VClusterOpEngine
+	 *   - Give the instructions to the VClusterOpEngine to run
+	 */
+
+	err := options.validateAnalyzeOptions()
+	if err != nil {
+		return err
+	}
+
+	var vdb VCoordinationDatabase
+	// TODO: do we need to separte the case of main cluster and sandboxes?
+	err = vcc.getVDBFromRunningDBIncludeSandbox(&vdb, &options.DatabaseOptions, util.MainClusterSandbox)
+	if err != nil {
+		return err
+	}
+
+	instructions, err := vcc.produceSetRestartPolicyInstructions(options, &vdb)
+	if err != nil {
+		return err
+	}
+
+	// create a VClusterOpEngine for pre-check, and add certs to the engine
+	clusterOpEngine := makeClusterOpEngine(instructions, options)
+	runError := clusterOpEngine.run(vcc.Log)
+	if runError != nil {
+		return fmt.Errorf("failed to set restart policy: %w", runError)
+	}
+
+	return nil
+}
+
+func (vcc VClusterCommands) produceSetRestartPolicyInstructions(
+	options *VSetRestartPolicyOptions,
+	vdb *VCoordinationDatabase) ([]clusterOp, error) {
+	var instructions []clusterOp
+
+	// TODO: do we need to separte the case of main cluster and sandboxes?
+	upHosts := vdb.filterUpHostListBySandbox(vdb.HostList, util.MainClusterSandbox)
+
+	setRestartPolicyOp, err := makeNMASetRestartPolicyOp(upHosts,
+		options.UserName, options.DBName, options.Password, options.Policy)
+	if err != nil {
+		return nil, err
+	}
+
+	instructions = append(instructions, &setRestartPolicyOp)
+
+	return instructions, nil
+}
